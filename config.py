eca1f814de6dfa1b0ae353b76749871fb5614e70
sonoden <dalessiodennis@gmail.com>
2026-03-12 15:31:29 +0100

    Add configuration file for Discord and Reddit settings

---
+++ config.py
@@ -0,0 +1,30 @@
+# Configuration settings for Discord and Reddit
+
+# Discord Configuration
+DISCORD_TOKEN = 'your_discord_token'
+CHANNEL_ID = 'your_channel_id'
+
+# Reddit Configuration
+REDDIT_CLIENT_ID = 'your_reddit_client_id'
+REDDIT_CLIENT_SECRET = 'your_reddit_client_secret'
+REDDIT_USER_AGENT = 'your_reddit_user_agent'
+
+# Heroes List
+HEROES = [
+    'Hero1',
+    'Hero2',
+    'Hero3',
+]
+
+# Rarity Levels
+RARITY_LEVELS = {
+    'Common': 1,
+    'Uncommon': 2,
+    'Rare': 3,
+    'Epic': 4,
+    'Legendary': 5
+}
+
+# Database Settings
+DB_URI = 'your_database_uri'
+DB_NAME = 'your_database_name'
